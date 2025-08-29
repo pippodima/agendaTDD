2f65ac577c1a76ba9e61e3b8762a90e61c0ad8fa
pippodima <pippodima99@gmail.com>
2025-08-29 20:07:25 +0200

    added testFindAllWhenDBIsEmpty red state

--- agenda/src/test/java/dimartinofilippo/agenda/repository/mongo/ToDoMongoRepositoryTest.java
+++ agenda/src/test/java/dimartinofilippo/agenda/repository/mongo/ToDoMongoRepositoryTest.java
@@ -1,18 +1,31 @@
 package dimartinofilippo.agenda.repository.mongo;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.net.InetSocketAddress;
 
 import org.bson.Document;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
-import com.mongodb.MongoClient;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 import de.bwaldvogel.mongo.MongoServer;
 import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
+import dimartinofilippo.agenda.model.ToDo;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ToDoMongoRepositoryTest {
+	
+    private static final String TODO_TITLE = "test";
+    private static final boolean TODO_DONE = false;
 	
 	private static MongoServer server;
 	private static InetSocketAddress serverAddress;
@@ -33,5 +46,27 @@ public class ToDoMongoRepositoryTest {
 		server.shutdown();
 	}
 	
+    @BeforeEach
+    void setup() {
+        String uri = "mongodb://" + serverAddress.getHostName() + ":" + serverAddress.getPort();
+        client = MongoClients.create(uri);
+
+        todoRepository = new ToDoMongoRepository(client);
+        MongoDatabase database = client.getDatabase(ToDoMongoRepository.AGENDA_DB_NAME);
+        database.drop();
+        todoCollection = database.getCollection(ToDoMongoRepository.TODO_COLLECTION_NAME);
+    }
+    
+    @AfterEach
+    void tearDownClient() {
+    	client.close();
+    }
+    
+    @Test
+    public void testFindAllWhenDBIsEmpty() {
+    	assertThat(todoRepository.findAll().isEmpty());
+    
+    }
+
 
 }
