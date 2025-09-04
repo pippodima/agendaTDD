2ed7a2b82602650903304962f6a4b694c6e5dabd
pippodima <pippodima99@gmail.com>
2025-09-04 14:58:32 +0200

    ToDoSwingViewIT class skeleton

--- agenda/src/it/java/dimartinofilippo/agenda/view/swing/ToDoSwingViewIT.java
+++ agenda/src/it/java/dimartinofilippo/agenda/view/swing/ToDoSwingViewIT.java
@@ -1,8 +1,29 @@
 package dimartinofilippo.agenda.view.swing;
 
+import java.net.InetSocketAddress;
+
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import de.bwaldvogel.mongo.MongoServer;
+import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
 
 public class ToDoSwingViewIT extends AssertJSwingJUnitTestCase{
+	
+	private static MongoServer server;
+	private static InetSocketAddress serverAddress;
+
+	@BeforeAll
+	static void setupServer() {
+		server = new MongoServer(new MemoryBackend());
+		serverAddress = server.bind();
+	}
+	
+	@AfterAll
+	static void shutDownServer() {
+		server.shutdown();
+	}
 
 	@Override
 	protected void onSetUp() throws Exception {
