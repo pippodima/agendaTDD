74e057142c00c86f1d35a93a35421ef0bf06d087
pippodima <pippodima99@gmail.com>
2025-09-04 14:54:04 +0200

    created swingViewIt

---
+++ agenda/src/it/java/dimartinofilippo/agenda/view/swing/ToDoSwingViewIT.java
@@ -0,0 +1,13 @@
+package dimartinofilippo.agenda.view.swing;
+
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+
+public class ToDoSwingViewIT extends AssertJSwingJUnitTestCase{
+
+	@Override
+	protected void onSetUp() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
