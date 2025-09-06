fb8966bb96ff1b4d1906d60967a5758f39247b75
pippodima <pippodima99@gmail.com>
2025-09-06 12:10:07 +0200

    addButton e2e test

--- agenda/src/e2e/java/dimartinofilippo/agenda/view/swing/AgendaSwingAppE2E.java
+++ agenda/src/e2e/java/dimartinofilippo/agenda/view/swing/AgendaSwingAppE2E.java
@@ -22,75 +22,106 @@ import com.mongodb.client.MongoClients;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AgendaSwingAppE2E {
 
-    @SuppressWarnings("rawtypes")
-    public static final GenericContainer mongo =
-            new GenericContainer("mongo:4.0.5").withExposedPorts(27017);
+	@SuppressWarnings("rawtypes")
+	public static final GenericContainer mongo = new GenericContainer("mongo:4.0.5").withExposedPorts(27017);
 
-    private static final String DB_NAME = "agenda";
-    private static final String COLLECTION_NAME = "todos";
+	private static final String DB_NAME = "agenda";
+	private static final String COLLECTION_NAME = "todos";
 
-    private static Robot robot;
+	private static Robot robot;
 
-    private MongoClient mongoClient;
-    private FrameFixture window;
+	private MongoClient mongoClient;
+	private FrameFixture window;
 
-    @BeforeAll
-    static void setUpRobot() {
-        robot = BasicRobot.robotWithNewAwtHierarchy();
-        robot.settings().delayBetweenEvents(50);
-    }
+	@BeforeAll
+	static void setUpRobot() {
+		robot = BasicRobot.robotWithNewAwtHierarchy();
+		robot.settings().delayBetweenEvents(50);
+	}
 
-    @AfterAll
-    static void tearDownRobot() {
-        if (robot != null) {
-            robot.cleanUp();
-        }
-    }
+	@AfterAll
+	static void tearDownRobot() {
+		if (robot != null) {
+			robot.cleanUp();
+		}
+	}
 
-    @BeforeEach
-    void setUp() {
-        mongo.start();
+	@BeforeEach
+	void setUp() {
+		mongo.start();
 
-        String containerIpAddress = mongo.getHost();
-        Integer mappedPort = mongo.getMappedPort(27017);
+		String containerIpAddress = mongo.getHost();
+		Integer mappedPort = mongo.getMappedPort(27017);
 
-        mongoClient = MongoClients.create("mongodb://" + containerIpAddress + ":" + mappedPort);
-        mongoClient.getDatabase(DB_NAME).drop();
+		mongoClient = MongoClients.create("mongodb://" + containerIpAddress + ":" + mappedPort);
+		mongoClient.getDatabase(DB_NAME).drop();
 
-        addTestTodoToDatabase("todo1", true);
-        addTestTodoToDatabase("todo2", false);
+		addTestTodoToDatabase("todo1", true);
+		addTestTodoToDatabase("todo2", false);
 
-        application("dimartinofilippo.agenda.App").withArgs("mongo", containerIpAddress, String.valueOf(mappedPort), DB_NAME).start();
+		application("dimartinofilippo.agenda.App")
+				.withArgs("mongo", containerIpAddress, String.valueOf(mappedPort), DB_NAME).start();
 
-        window = WindowFinder.findFrame(new GenericTypeMatcher<JFrame>(JFrame.class) {
-            @Override
-            protected boolean isMatching(JFrame frame) {
-                return "Agenda - ToDo List".equals(frame.getTitle()) && frame.isShowing();
-            }
-        }).using(robot);
-    }
+		window = WindowFinder.findFrame(new GenericTypeMatcher<JFrame>(JFrame.class) {
+			@Override
+			protected boolean isMatching(JFrame frame) {
+				return "Agenda - ToDo List".equals(frame.getTitle()) && frame.isShowing();
+			}
+		}).using(robot);
+	}
 
-    @AfterEach
-    void tearDown() {
-        if (mongoClient != null) {
-            mongoClient.close();
-        }
-        mongo.stop();
-        if (window != null) {
-            window.cleanUp();
-        }
-    }
+	@AfterEach
+	void tearDown() {
+		if (mongoClient != null) {
+			mongoClient.close();
+		}
+		mongo.stop();
+		if (window != null) {
+			window.cleanUp();
+		}
+	}
 
-    @Test
-    void testOnStartAllDBElementsAreShown() {
-        assertThat(window.list().contents())
-            .anySatisfy(e -> assertThat(e).contains(new ToDo("todo1", true).toString()))
-            .anySatisfy(e -> assertThat(e).contains(new ToDo("todo2", false).toString()));
-    }
+	@Test
+	void testOnStartAllDBElementsAreShown() {
+		assertThat(window.list().contents()).anySatisfy(e -> assertThat(e).contains(new ToDo("todo1", true).toString()))
+				.anySatisfy(e -> assertThat(e).contains(new ToDo("todo2", false).toString()));
+	}
 
-    // helper
-    private void addTestTodoToDatabase(String title, boolean done) {
-        mongoClient.getDatabase(DB_NAME).getCollection(COLLECTION_NAME)
-            .insertOne(new Document().append("title", title).append("done", done));
-    }
+	@Test
+	void testAddButtonSuccess() {
+		window.textBox("titleTextBox").enterText("addButtonSuccess");
+		window.checkBox("doneCheckBox").uncheck();
+		window.button("addButton").click();
+
+		assertThat(window.list().contents())
+				.anySatisfy(e -> assertThat(e).contains(new ToDo("addButtonSuccess", false).toString()));
+	}
+
+	@Test
+	void testAddButtonErrorForToDoAlreadyExisting() {
+		window.textBox("titleTextBox").enterText("todo2");
+		window.checkBox("doneCheckBox").uncheck();
+		window.button("addButton").click();
+
+		assertThat(window.list("todoList").contents()).containsExactly(new ToDo("todo1", true).toString(),
+				new ToDo("todo2", false).toString());
+
+		window.label("errorMessageLabel").requireText("same ToDo already in the agenda: todo2");
+
+	}
+
+	@Test
+	void testAddButtonErrorForToDoWithoutTitle() {
+		window.checkBox("doneCheckBox").uncheck();
+		window.button("addButton").click();
+		assertThat(window.list("todoList").contents()).containsExactly(new ToDo("todo1", true).toString(),
+				new ToDo("todo2", false).toString());
+		window.label("errorMessageLabel").requireText(" ");
+	}
+
+	// helper
+	private void addTestTodoToDatabase(String title, boolean done) {
+		mongoClient.getDatabase(DB_NAME).getCollection(COLLECTION_NAME)
+				.insertOne(new Document().append("title", title).append("done", done));
+	}
 }
