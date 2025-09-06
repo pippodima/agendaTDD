44b60cbbfb3e7a8bc32877eb2518c59f32eb4855
pippodima <pippodima99@gmail.com>
2025-09-06 12:51:51 +0200

    deleteButton e2e test

--- agenda/src/e2e/java/dimartinofilippo/agenda/view/swing/AgendaSwingAppE2E.java
+++ agenda/src/e2e/java/dimartinofilippo/agenda/view/swing/AgendaSwingAppE2E.java
@@ -7,6 +7,7 @@ import static org.assertj.swing.launcher.ApplicationLauncher.*;
 
 import javax.swing.JFrame;
 
+import org.assertj.swing.annotation.GUITest;
 import org.assertj.swing.core.BasicRobot;
 import org.assertj.swing.core.GenericTypeMatcher;
 import org.assertj.swing.core.Robot;
@@ -18,6 +19,7 @@ import org.junit.jupiter.api.*;
 import org.testcontainers.containers.GenericContainer;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.model.Filters;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AgendaSwingAppE2E {
@@ -82,12 +84,14 @@ public class AgendaSwingAppE2E {
 	}
 
 	@Test
+	@GUITest
 	void testOnStartAllDBElementsAreShown() {
 		assertThat(window.list().contents()).anySatisfy(e -> assertThat(e).contains(new ToDo("todo1", true).toString()))
 				.anySatisfy(e -> assertThat(e).contains(new ToDo("todo2", false).toString()));
 	}
 
 	@Test
+	@GUITest
 	void testAddButtonSuccess() {
 		window.textBox("titleTextBox").enterText("addButtonSuccess");
 		window.checkBox("doneCheckBox").uncheck();
@@ -98,6 +102,7 @@ public class AgendaSwingAppE2E {
 	}
 
 	@Test
+	@GUITest
 	void testAddButtonErrorForToDoAlreadyExisting() {
 		window.textBox("titleTextBox").enterText("todo2");
 		window.checkBox("doneCheckBox").uncheck();
@@ -111,6 +116,7 @@ public class AgendaSwingAppE2E {
 	}
 
 	@Test
+	@GUITest
 	void testAddButtonErrorForToDoWithoutTitle() {
 		window.checkBox("doneCheckBox").uncheck();
 		window.button("addButton").click();
@@ -119,9 +125,40 @@ public class AgendaSwingAppE2E {
 		window.label("errorMessageLabel").requireText(" ");
 	}
 
+	@Test
+	@GUITest
+	void testDeleteButtonSuccess() {
+		window.list("todoList").selectItem(0);
+		window.button("deleteButton").click();
+
+		assertThat(window.list("todoList").contents()).containsExactly(new ToDo("todo2", false).toString());
+	}
+
+	@Test
+	@GUITest
+	void testDeleteButtonError() {
+		window.list("todoList").selectItem(0);
+
+		deleteTestTodoToDatabase("todo1");
+		window.button("deleteButton").click();
+		window.label("errorMessageLabel").requireText("ToDo doesn't exist: todo1");
+
+	}
+
+	@Test
+	@GUITest
+	void testDeleteButtonErrorToDoNotSelected() {
+		window.button("deleteButton").click();
+		window.label("errorMessageLabel").requireText(" ");
+	}
+
 	// helper
 	private void addTestTodoToDatabase(String title, boolean done) {
 		mongoClient.getDatabase(DB_NAME).getCollection(COLLECTION_NAME)
 				.insertOne(new Document().append("title", title).append("done", done));
+	}
+
+	private void deleteTestTodoToDatabase(String title) {
+		mongoClient.getDatabase(DB_NAME).getCollection(COLLECTION_NAME).deleteOne(Filters.eq("title", title));
 	}
 }
