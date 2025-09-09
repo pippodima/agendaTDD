f577314308bbf92bc463ac4264a7b8e53701634e
pippodima <pippodima99@gmail.com>
2025-09-09 16:41:30 +0200

    added coverall token

--- agenda/src/test/java/dimartinofilippo/agenda/repository/sql/ToDoSQLRepositoryTest.java
+++ agenda/src/test/java/dimartinofilippo/agenda/repository/sql/ToDoSQLRepositoryTest.java
@@ -26,150 +26,138 @@ import dimartinofilippo.agenda.model.ToDo;
 @ExtendWith(MockitoExtension.class)
 class ToDoSQLRepositoryTest {
 
-    @Mock
-    private DataSource dataSource;
+	@Mock
+	private DataSource dataSource;
 
-    @Mock
-    private Connection connection;
+	@Mock
+	private Connection connection;
 
-    @Mock
-    private PreparedStatement preparedStatement;
+	@Mock
+	private PreparedStatement preparedStatement;
 
-    @Mock
-    private Statement statement;
+	@Mock
+	private Statement statement;
 
-    @Mock
-    private ResultSet resultSet;
+	@Mock
+	private ResultSet resultSet;
 
-    @InjectMocks
-    private ToDoSQLRepository sqlRepository;
+	@InjectMocks
+	private ToDoSQLRepository sqlRepository;
 
-    @BeforeEach
-    void setup() throws Exception {
-        when(dataSource.getConnection()).thenReturn(connection);
-    }
+	@BeforeEach
+	void setup() throws Exception {
+		when(dataSource.getConnection()).thenReturn(connection);
+	}
 
-    @Test
-    void testFindAllWhenEmptyReturnEmptyList() throws Exception {
-        when(connection.prepareStatement(ToDoSQLRepository.SELECT_ALL))
-                .thenReturn(preparedStatement);
-        when(preparedStatement.executeQuery()).thenReturn(resultSet);
-        when(resultSet.next()).thenReturn(false);
+	@Test
+	void testFindAllWhenEmptyReturnEmptyList() throws Exception {
+		when(connection.prepareStatement(ToDoSQLRepository.SELECT_ALL)).thenReturn(preparedStatement);
+		when(preparedStatement.executeQuery()).thenReturn(resultSet);
+		when(resultSet.next()).thenReturn(false);
 
-        List<ToDo> result = sqlRepository.findAll();
+		List<ToDo> result = sqlRepository.findAll();
 
-        assertThat(result).isEmpty();
-    }
+		assertThat(result).isEmpty();
+	}
 
-    @Test
-    void testFindAllWhenNotEmptyReturnsRows() throws Exception {
-        when(connection.prepareStatement(ToDoSQLRepository.SELECT_ALL))
-                .thenReturn(preparedStatement);
-        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+	@Test
+	void testFindAllWhenNotEmptyReturnsRows() throws Exception {
+		when(connection.prepareStatement(ToDoSQLRepository.SELECT_ALL)).thenReturn(preparedStatement);
+		when(preparedStatement.executeQuery()).thenReturn(resultSet);
 
-        when(resultSet.next()).thenReturn(true, true, false);
-        when(resultSet.getString("title")).thenReturn("todo1", "todo2");
-        when(resultSet.getBoolean("done")).thenReturn(true, false);
+		when(resultSet.next()).thenReturn(true, true, false);
+		when(resultSet.getString("title")).thenReturn("todo1", "todo2");
+		when(resultSet.getBoolean("done")).thenReturn(true, false);
 
-        List<ToDo> result = sqlRepository.findAll();
+		List<ToDo> result = sqlRepository.findAll();
 
-        assertThat(result).containsExactly(
-                new ToDo("todo1", true),
-                new ToDo("todo2", false));
-    }
+		assertThat(result).containsExactly(new ToDo("todo1", true), new ToDo("todo2", false));
+	}
 
-    @Test
-    void testFindByTitle_notFound_returnsEmpty() throws Exception {
-        when(connection.prepareStatement(ToDoSQLRepository.SELECT_BY_TITLE))
-                .thenReturn(preparedStatement);
-        when(preparedStatement.executeQuery()).thenReturn(resultSet);
-        when(resultSet.next()).thenReturn(false);
+	@Test
+	void testFindByTitle_notFound_returnsEmpty() throws Exception {
+		when(connection.prepareStatement(ToDoSQLRepository.SELECT_BY_TITLE)).thenReturn(preparedStatement);
+		when(preparedStatement.executeQuery()).thenReturn(resultSet);
+		when(resultSet.next()).thenReturn(false);
 
-        Optional<ToDo> result = sqlRepository.findByTitle("missing");
+		Optional<ToDo> result = sqlRepository.findByTitle("missing");
 
-        assertThat(result).isEmpty();
-    }
+		assertThat(result).isEmpty();
+	}
 
-    @Test
-    void testFindByTitle_found_returnsOptional() throws Exception {
-        when(connection.prepareStatement(ToDoSQLRepository.SELECT_BY_TITLE))
-                .thenReturn(preparedStatement);
-        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+	@Test
+	void testFindByTitle_found_returnsOptional() throws Exception {
+		when(connection.prepareStatement(ToDoSQLRepository.SELECT_BY_TITLE)).thenReturn(preparedStatement);
+		when(preparedStatement.executeQuery()).thenReturn(resultSet);
 
-        when(resultSet.next()).thenReturn(true);
-        when(resultSet.getString("title")).thenReturn("task2");
-        when(resultSet.getBoolean("done")).thenReturn(true);
+		when(resultSet.next()).thenReturn(true);
+		when(resultSet.getString("title")).thenReturn("task2");
+		when(resultSet.getBoolean("done")).thenReturn(true);
 
-        Optional<ToDo> result = sqlRepository.findByTitle("task2");
+		Optional<ToDo> result = sqlRepository.findByTitle("task2");
 
-        assertThat(result).isPresent().contains(new ToDo("task2", true));
-        verify(preparedStatement).setString(1, "task2");
+		assertThat(result).isPresent().contains(new ToDo("task2", true));
+		verify(preparedStatement).setString(1, "task2");
 
-    }
+	}
 
-    @Test
-    void testSaveInsertRow() throws Exception {
-        ToDo todo = new ToDo("todo1", false);
+	@Test
+	void testSaveInsertRow() throws Exception {
+		ToDo todo = new ToDo("todo1", false);
 
-        when(connection.prepareStatement(ToDoSQLRepository.INSERT))
-                .thenReturn(preparedStatement);
+		when(connection.prepareStatement(ToDoSQLRepository.INSERT)).thenReturn(preparedStatement);
 
-        sqlRepository.save(todo);
+		sqlRepository.save(todo);
 
-        verify(preparedStatement).setString(1, "todo1");
-        verify(preparedStatement).setBoolean(2, false);
-        verify(preparedStatement).executeUpdate();
-    }
+		verify(preparedStatement).setString(1, "todo1");
+		verify(preparedStatement).setBoolean(2, false);
+		verify(preparedStatement).executeUpdate();
+	}
 
-    @Test
-    void testDeleteByTitle_removesRow() throws Exception {
-        when(connection.prepareStatement(ToDoSQLRepository.DELETE))
-                .thenReturn(preparedStatement);
+	@Test
+	void testDeleteByTitle_removesRow() throws Exception {
+		when(connection.prepareStatement(ToDoSQLRepository.DELETE)).thenReturn(preparedStatement);
 
-        sqlRepository.deleteByTitle("task1");
+		sqlRepository.deleteByTitle("task1");
 
-        verify(preparedStatement).setString(1, "task1");
-        verify(preparedStatement).executeUpdate();
-    }
-    
-    @Test
-    void testSave_whenSQLException_thenWrapsInRuntimeException() throws Exception {
-        when(connection.prepareStatement(anyString())).thenThrow(new SQLException("boom"));
+		verify(preparedStatement).setString(1, "task1");
+		verify(preparedStatement).executeUpdate();
+	}
 
-        ToDo todo = new ToDo("x", true);
+	@Test
+	void testSave_whenSQLException_thenWrapsInRuntimeException() throws Exception {
+		when(connection.prepareStatement(anyString())).thenThrow(new SQLException("boom"));
 
-        assertThatThrownBy(() -> sqlRepository.save(todo))
-                .isInstanceOf(RuntimeException.class)
-                .hasCauseInstanceOf(SQLException.class);
-    }
+		ToDo todo = new ToDo("x", true);
 
-    @Test
-    void testFindByTitle_whenSQLException_thenWrapsInRuntimeException() throws Exception {
-        when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
-        when(preparedStatement.executeQuery()).thenThrow(new SQLException("fail"));
+		assertThatThrownBy(() -> sqlRepository.save(todo)).isInstanceOf(RuntimeException.class)
+				.hasCauseInstanceOf(SQLException.class);
+	}
 
-        assertThatThrownBy(() -> sqlRepository.findByTitle("test"))
-                .isInstanceOf(RuntimeException.class)
-                .hasCauseInstanceOf(SQLException.class);
-    }
+	@Test
+	void testFindByTitle_whenSQLException_thenWrapsInRuntimeException() throws Exception {
+		when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
+		when(preparedStatement.executeQuery()).thenThrow(new SQLException("fail"));
 
-    @Test
-    void testFindAll_whenSQLException_thenWrapsInRuntimeException() throws Exception {
-        when(connection.prepareStatement(anyString())).thenThrow(new SQLException("nope"));
+		assertThatThrownBy(() -> sqlRepository.findByTitle("test")).isInstanceOf(RuntimeException.class)
+				.hasCauseInstanceOf(SQLException.class);
+	}
 
-        assertThatThrownBy(() -> sqlRepository.findAll())
-                .isInstanceOf(RuntimeException.class)
-                .hasCauseInstanceOf(SQLException.class);
-    }
+	@Test
+	void testFindAll_whenSQLException_thenWrapsInRuntimeException() throws Exception {
+		when(connection.prepareStatement(anyString())).thenThrow(new SQLException("nope"));
 
-    @Test
-    void testDeleteByTitle_whenSQLException_thenWrapsInRuntimeException() throws Exception {
-        when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
-        doThrow(new SQLException("bad delete")).when(preparedStatement).executeUpdate();
+		assertThatThrownBy(() -> sqlRepository.findAll()).isInstanceOf(RuntimeException.class)
+				.hasCauseInstanceOf(SQLException.class);
+	}
 
-        assertThatThrownBy(() -> sqlRepository.deleteByTitle("test"))
-                .isInstanceOf(RuntimeException.class)
-                .hasCauseInstanceOf(SQLException.class);
-    }
-    
+	@Test
+	void testDeleteByTitle_whenSQLException_thenWrapsInRuntimeException() throws Exception {
+		when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
+		doThrow(new SQLException("bad delete")).when(preparedStatement).executeUpdate();
+
+		assertThatThrownBy(() -> sqlRepository.deleteByTitle("test")).isInstanceOf(RuntimeException.class)
+				.hasCauseInstanceOf(SQLException.class);
+	}
+
 }
