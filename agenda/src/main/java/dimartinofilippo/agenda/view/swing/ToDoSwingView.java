e0e4e0c08bca490507d90d7bbde79ecaa87e0ca9
pippodima <pippodima99@gmail.com>
2025-09-10 16:42:27 +0200

    removed unused imports

--- agenda/src/main/java/dimartinofilippo/agenda/view/swing/ToDoSwingView.java
+++ agenda/src/main/java/dimartinofilippo/agenda/view/swing/ToDoSwingView.java
@@ -21,8 +21,6 @@ import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 
 import dimartinofilippo.agenda.controller.AgendaController;
 import dimartinofilippo.agenda.model.ToDo;
